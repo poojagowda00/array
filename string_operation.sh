b57bed75b95001a41ba7a3254393e325206d5d44
EC2 Default User <ec2-user@ip-172-31-1-118.us-west-1.compute.internal>
2024-01-01 11:39:05 +0000

    commit the file

--- string_operation.sh
+++ string_operation.sh
@@ -4,15 +4,25 @@
 
 myvar="Hey POOJA GOWDA Welcome !!!!!!!!! How are you :"
  myvarlength=${#myvar}
-echo "Length of the myvar is $myvarlength "
+echo "Length of the myvar is.................................... $myvarlength "
 
 #using upper case
 
 
-echo " upper case is ${myvar^^}"
+echo " upper case is ..............................${myvar^^}"
 
 #lower case
 
 
 
-echo " lower case is ${myvar,,}"
+echo " lower case is......................... ${myvar,,}"
+
+#To replace a string
+
+newmyvar=${myvar/Hey/ok}
+
+echo " new string from myvar word hey will get replace by ok..........................$newmyvar"
+
+#To get a slice from a string
+
+echo " Slice of a word ${myvar:4:5}"
